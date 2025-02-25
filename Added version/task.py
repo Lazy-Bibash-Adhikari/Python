0982e0696959725ff2f76401ea1ea769b73d333a
Lazy-Bibash-Adhikari <lazytenno1211@gmail.com>
2025-02-25 13:36:58 -0500

    a

--- Added version/task.py
+++ Added version/task.py
@@ -4,7 +4,7 @@ class Task:
         self.title = title
         self.completed = completed
 
-    
+
     def __str__(self):
         status = "Completed" if self.completed else "Not Completed"
         return f"ID: {self.id}, Title: {self.title}, Status: {status}"
